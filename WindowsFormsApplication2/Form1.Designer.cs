e7830841efb855d92bea10f9d99c174bbc2537b7
Artem <rollmaxidom@gmail.com>
2017-01-01 18:25:49 +0100

    initial commit eyesight

---
+++ WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class FormWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.labelCountDownWork = new System.Windows.Forms.Label();
+            this.btnTimerWorkCycle = new System.Windows.Forms.Button();
+            this.timerWindowApp = new System.Windows.Forms.Timer(this.components);
+            this.timerDisplayCountDown = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // labelCountDownWork
+            // 
+            this.labelCountDownWork.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelCountDownWork.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCountDownWork.Location = new System.Drawing.Point(0, 0);
+            this.labelCountDownWork.Margin = new System.Windows.Forms.Padding(3, 100, 3, 0);
+            this.labelCountDownWork.Name = "labelCountDownWork";
+            this.labelCountDownWork.Size = new System.Drawing.Size(484, 461);
+            this.labelCountDownWork.TabIndex = 0;
+            this.labelCountDownWork.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnTimerWorkCycle
+            // 
+            this.btnTimerWorkCycle.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnTimerWorkCycle.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTimerWorkCycle.Location = new System.Drawing.Point(188, 116);
+            this.btnTimerWorkCycle.Name = "btnTimerWorkCycle";
+            this.btnTimerWorkCycle.Size = new System.Drawing.Size(100, 25);
+            this.btnTimerWorkCycle.TabIndex = 1;
+            this.btnTimerWorkCycle.Text = "start timer";
+            this.btnTimerWorkCycle.UseVisualStyleBackColor = true;
+            this.btnTimerWorkCycle.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timerWindowApp
+            // 
+            this.timerWindowApp.Interval = 5000;
+            this.timerWindowApp.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timerDisplayCountDown
+            // 
+            this.timerDisplayCountDown.Interval = 1000;
+            this.timerDisplayCountDown.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // FormWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 461);
+            this.Controls.Add(this.btnTimerWorkCycle);
+            this.Controls.Add(this.labelCountDownWork);
+            this.Name = "FormWindow";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelCountDownWork;
+        private System.Windows.Forms.Button btnTimerWorkCycle;
+        private System.Windows.Forms.Timer timerWindowApp;
+        private System.Windows.Forms.Timer timerDisplayCountDown;
+    }
+}
+
---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+
+    public partial class FormWindow : Form
+    {
+        public FormWindow()
+        {
+            InitializeComponent();    
+        }
+        class Globals
+        {
+            public static int setTimeMinutes;
+            public const int minuteTime = 1000 * 60;
+            public static int initCountDown;
+
+            public const string titleBarText = "EyeSight";
+            public const string pauseText= "Pause";
+            public const string startText = "Start";
+            public const string timeLeftText = " minutes left";
+            public const string pauseFinishedText = "Pause is finished";
+            public const string messagePauseText = "Pause?";
+            public const string messagePauseTitle = "Pause?";
+            public static bool pauseCycleRunning = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            timerDisplayCountDown.Interval = Globals.minuteTime;
+            //labelCurrentDate.Text = DateTime.Now.ToString(("hh:mm:ss tt"));
+
+            this.Text = Globals.titleBarText;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            startTimerWorkCycle();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timerDisplayCountDown.Stop();
+            timerWindowApp.Stop();
+            this.CenterToScreen();
+            if (this.WindowState == FormWindowState.Minimized) { 
+                this.WindowState = FormWindowState.Normal;
+            }
+            this.TopMost = true;
+            if (Globals.pauseCycleRunning == false)
+            {
+                labelCountDownWork.Text = Globals.setTimeMinutes.ToString();
+                btnTimerWorkCycle.Text = Globals.startText;
+            }
+            else
+            {
+                labelCountDownWork.Text = Globals.pauseFinishedText.ToString();
+            }
+            if (Globals.pauseCycleRunning == false) { 
+                DialogResult msgPause = MessageBox.Show(
+                    Globals.messagePauseTitle, Globals.messagePauseText, MessageBoxButtons.YesNo);
+                if (msgPause == DialogResult.Yes)
+                {
+                    startTimerPauseCycle();
+                }
+                else if ( msgPause == DialogResult.No ) {
+                    startTimerWorkCycle();
+                }
+            }
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            Globals.initCountDown -=  1;
+            labelCountDownWork.Text = Globals.initCountDown.ToString() + Globals.timeLeftText;
+        }
+        private void startTimerWorkCycle() {
+            Globals.pauseCycleRunning = false;
+            setTimeInterval(false);
+            timerWindowApp.Interval = Globals.setTimeMinutes * Globals.minuteTime;
+            btnTimerWorkCycle.Text = Globals.pauseText;
+            labelCountDownWork.Text = Globals.initCountDown.ToString() + Globals.timeLeftText;
+            this.TopMost = false;
+            timerDisplayCountDown.Start();
+            timerWindowApp.Start();
+
+        }
+        private void startTimerPauseCycle()
+        {
+            Globals.pauseCycleRunning = true;
+            setTimeInterval(true);
+            timerWindowApp.Interval = Globals.setTimeMinutes * Globals.minuteTime;
+            btnTimerWorkCycle.Text = Globals.pauseText;
+            labelCountDownWork.Text = Globals.initCountDown.ToString() + Globals.timeLeftText;
+            this.TopMost = false;
+            timerDisplayCountDown.Start();
+            timerWindowApp.Start();
+
+        }
+        private void setTimeInterval(bool pause) {
+            if (pause == false)
+            {
+                Globals.setTimeMinutes = 3;
+            }
+            else
+            {
+                Globals.setTimeMinutes = 2;
+            }
+            int time = Globals.setTimeMinutes;
+            Globals.initCountDown = time;
+        }
+    }
+
+}
